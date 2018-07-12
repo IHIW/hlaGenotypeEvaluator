acc2f3d00b9ad37fc17915ce4f66dba9ee134c91
Kazutoyo Osoegawa <kazutoyo@stanford.edu>
2018-07-12 13:50:55 -0700

    Driver

---
+++ src/main/java/workshop/panel/driver/DriverForGenerateCompareResultsTable.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package workshop.panel.driver;
+
+import workshop.panel.write.GenerateCompareResultsTable;
+
+/**
+ * @author kazu
+ * @version January 20 2017
+ *
+ */
+public class DriverForGenerateCompareResultsTable {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new GenerateCompareResultsTable(args[0], args[1], args[2]);
+
+	}
+
+}
