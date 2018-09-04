c9f5c74e67185f1638436bbc1d7b1f116103ff10
Kazutoyo Osoegawa <kazutoyo@stanford.edu>
2018-09-04 14:03:49 -0700

    update driverForRunValidation.sh

--- driverForRunValidation.sh
+++ driverForRunValidation.sh
@@ -3,7 +3,7 @@
 if [ "$#" -ne 1 ]; then
   echo "Illegal number of parameters"
 else
-  mvn exec:java -Dexec.mainClass="software.validation.driver.DriverForRunValidation" -Dexec.args="$*"
+  mvn exec:java -Dexec.mainClass="workshop.panel.driver.DriverForRunValidation" -Dexec.args="$*"
 fi
 
 exit 0
